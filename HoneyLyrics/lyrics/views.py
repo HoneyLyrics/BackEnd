ad9460073b3c8c3e2cf0ba4185df221e3e946bdf
kimSooHyun950921 <soohyunkim950921@gmail.com>
2021-06-15 20:12:26 +0900

    HL-21 Make Post Data

--- HoneyLyrics/lyrics/views.py
+++ HoneyLyrics/lyrics/views.py
@@ -1,10 +1,31 @@
 from json.encoder import JSONEncoder
 from django.views import View
 from django.http.response import JsonResponse, HttpResponse
-from django.shortcuts import render
-
+from lyrics.models import SongInfo, Lyrics, Mood
+import json
 # , redirect, get_list_or_404, get_object_or_404
-# from melonapi import scrapeMelon
+
+class Crawler(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        data = json.loads(request.body)
+        # TODO Data predict code 넣기
+        for song_info in data:
+            song = SongInfo(songId=song_info['songId'], 
+                             title=song_info['title'],
+                             artist=song_info['artists'],
+                             mood1 = Mood.objects.get(moodId=1),
+                             mood2 = Mood.objects.get(moodId=2),
+                             mood3 = Mood.objects.get(moodId=3))
+
+            lyric = Lyrics(songId=SongInfo.objects.get(songId=song_info['songId']), 
+                           content=song_info['lyrics'])
+            song.save()
+            lyric.save()
+
+        return HttpResponse("OK")
 
 # Create your views here.
 
