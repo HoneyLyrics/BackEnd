0fe00d8a3168c4d8f828e2d5a9388c416376f6a9
chelseashin <chaewonshin95@gmail.com>
2021-06-18 14:40:43 +0900

    HL-62 add mood items to json

--- HoneyLyrics/lyrics/views.py
+++ HoneyLyrics/lyrics/views.py
@@ -1,40 +1,44 @@
-from json.encoder import JSONEncoder
 from django.views import View
-from django.http.response import JsonResponse, HttpResponse
+from django.http.response import HttpResponse
 from lyrics.models import SongInfo, Lyrics, Mood
 import json
 from django.db.models import Q
-# , redirect, get_list_or_404, get_object_or_404
 
 
 class Song(View):
+    """ Create SongInfo by GET method
+
+        returns:
+            HttpResponse
+    """
     def get(self, request):
         data = []
-
         if request.GET.get('songid', False):
             song_id = request.GET['songid']
-            print("songid", song_id)
             all_entries = SongInfo.objects.filter(songId=song_id)
             for all_entry in all_entries:
-                print(all_entry.songId)
                 data.append({
                     'songId': all_entry.songId,
                     'singer': all_entry.artist,
                     'imgURL': all_entry.imgURL,
                     'title': all_entry.title,
+                    'mood1': all_entry.mood1.moodId,
+                    'mood2': all_entry.mood2.moodId,
+                    'mood3': all_entry.mood3.moodId, 
                 })
         json_data = json.dumps(data, ensure_ascii=False).encode('utf-8')
         return HttpResponse(json_data, content_type="application/json")
 
 
 class Crawler(View):
-    def get(self, request):
-        pass
+    """ Get data from Models
 
+        returns:
+            HttpResponse
+    """
     def post(self, request):
         data = json.loads(request.body)
         # TODO Data predict code 넣기
-        
         for song_info in data:
             song = SongInfo(songId=song_info['songId'],
                             title=song_info['title'],
@@ -44,18 +48,19 @@ class Crawler(View):
                             mood2=Mood.objects.get(moodId=song_info['mood2']),
                             mood3=Mood.objects.get(moodId=song_info['mood3'])
                             )
-
             lyric = Lyrics(songId=SongInfo.objects.get(songId=song_info['songId']), 
                            content=song_info['lyrics'])
             song.save()
             lyric.save()
-
         return HttpResponse("OK")
 
-# Create your views here.
 
 class MusicList(View):
-    # GET Data
+    """ Create MusicList by GET method
+
+        returns:
+            HttpResponse
+    """
     def get(self, request):
         data = []
         if request.GET.get('moodid', False):
@@ -65,8 +70,7 @@ class MusicList(View):
                 Q(mood2=Mood.objects.get(moodId=mood))|
                 Q(mood3=Mood.objects.get(moodId=mood))
             )
-            
-            #print(all_entries)
+
             for all_entry in all_entries:
                 lyrics = Lyrics.objects.filter(
                          songId=SongInfo.objects.get(songId=all_entry.songId)
@@ -82,10 +86,5 @@ class MusicList(View):
         return HttpResponse(json_data, content_type="application/json")
 
 
-    # post Data
     def musiclist(self, request):
         return HttpResponse("OK")
-
-
-    
-
